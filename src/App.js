e617614894d8d5edf8216d6293c16eac4ab21787
Yumalup <sonaki970811@gmail.com>
2022-08-16 22:45:56 +0900

    Add : study

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import { useEffect, useState } from "react";
 import Button from "./Button.js";
 import styles from "./App.module.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("i run all the time");
+  useEffect(() => {
+    console.log("Call the API");
+  }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log(" Search for", keyword);
+    }
+  }, [keyword]);
   return (
     <div>
-      <h1 className={styles.title}>Welcome Back!!</h1>
-      <Button text={"Continue"} />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
